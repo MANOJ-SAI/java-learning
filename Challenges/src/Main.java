8c1f22ce8bcf8fb034b79aef57449c21efde2bb1
Sai <manojsai821@gmail.com>
2024-03-30 20:06:20 +0530

    Challenges: update OperatorPrecedenceChallenge

--- Challenges/src/Main.java
+++ Challenges/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        (new OperatorPrecedenceChallenge(20.00,80.00)).displayResult();
+        (new OperatorPrecedenceChallenge(21.00,80.00)).displayResult();
 
     }
 }
--- Challenges/src/OperatorPrecedenceChallenge.java
+++ Challenges/src/OperatorPrecedenceChallenge.java
@@ -12,15 +12,16 @@ public class OperatorPrecedenceChallenge {
     OperatorPrecedenceChallenge(double value1,double value2){
         this.value1 = value1;
         this.value2 = value2;
-        this.result = (this.value1 + this.value2) * 100.00;
-        this.remainder = this.result % 40.00;
-        this.value3 = this.remainder == 0.00 ;
+        this.result = (this.value1 + this.value2) * 100.00d;
+        this.remainder = this.result % 40.00d;
+        this.value3 = this.remainder == 0 ? true : false ;
     }
 
     public void displayResult(){
         System.out.println("result = "+this.result);
         System.out.println("remainder = "+this.remainder);
-        System.out.println("boolean value = "+this.value3);
+        if(!this.value3)
+            System.out.println("Got some remainder "+this.remainder);
     }
 
 
