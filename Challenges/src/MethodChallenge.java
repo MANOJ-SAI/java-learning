b6a3717f7b2e2245c68c9d1301a0d76f93d209ef
Sai <manojsai821@gmail.com>
2024-03-30 22:24:36 +0530

    MethodChallenge: add MethodChallenge project

---
+++ Challenges/src/MethodChallenge.java
@@ -0,0 +1,17 @@
+public class MethodChallenge {
+    public static void displayHighScorePosition(String playerName, int playerPosition) {
+        System.out.println(playerName + " managed to get position " + playerPosition + " on the high score list");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        int position = 4;
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
+        }
+        return position;
+    }
+}
