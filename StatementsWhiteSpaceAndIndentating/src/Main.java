dbccdba93ded1a4a8567711534ef3bad1dfc709f
Sai <manojsai821@gmail.com>
2024-03-30 21:00:57 +0530

    StatementsWhiteSpaceAndIndentating: add StatementsWhiteSpaceAndIndentating project

---
+++ StatementsWhiteSpaceAndIndentating/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int value1 = 50;
+
+        value1++;
+        value1--;
+
+        System.out.println(value1);
+
+        int value2 = 50;
+        value2++;
+        value2--;
+        System.out.println(value2);
+    }
+}
